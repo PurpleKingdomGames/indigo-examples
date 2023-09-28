57745a4025398e2d0d820319fd184c2d568d9ca5
Dave Smith <davesmith.00000@gmail.com>
2023-09-28 08:34:39 +0100

    Removed how-to's

--- howto/parcel/gamestart.js
+++
@@ -1,3 +0,0 @@
-var indigo = require("/target/scala-3.2.0/helloindigo-fastopt.js");
-
-indigo.IndigoGame.launch(document.getElementById("indigo-container"), {"width": window.innerWidth.toString(), "height": window.innerHeight.toString()})
--- howto/parcel/project/plugins.sbt
+++
@@ -1,2 +0,0 @@
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.11.0")
-addSbtPlugin("io.indigoengine" %% "sbt-indigo" % "0.14.0")
